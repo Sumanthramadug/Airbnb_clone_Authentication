5cdf91ad68fe824b962c57495c596b5a931ecaed
Suman <104339853+Sumanthramadug@users.noreply.github.com>
2025-05-12 08:16:11 +0530

    Update ResetPassword.js

--- frontend/src/Pages/ResetPassword.js
+++ frontend/src/Pages/ResetPassword.js
@@ -15,7 +15,7 @@ const ResetPassword = () => {
     e.preventDefault();
     try {
       const { data } = await axios.post(
-        'http://localhost:8000/users/forgotPassword',
+        'https://airbnb-clone-authentication.onrender.com/users/forgotPassword',
         { email },
         { withCredentials: true }
       );
@@ -33,7 +33,7 @@ const ResetPassword = () => {
   const checkOtpHandler = async () => {
     try {
       const { data } = await axios.post(
-        'http://localhost:8000/users/verifyotp',
+        'https://airbnb-clone-authentication.onrender.com/users/verifyotp',
         { email, otp },
         { withCredentials: true }
       );
